c6b921a7d89efb95c242888858a583ea6ca4e799
Luke Corbett <corbettluke14@gmail.com>
2025-03-05 10:14:26 -0700

    Formatted with Prettier

--- FirstReact/frontend/src/App.tsx
+++ FirstReact/frontend/src/App.tsx
@@ -1,49 +1,52 @@
-import './App.css'
-import teams from './CollegeBasketballTeams.json'
+import './App.css';
+import teams from './CollegeBasketballTeams.json';
 
-const teamNames = teams.teams
-
+const teamNames = teams.teams;
 
 function Welcome() {
-  return (
-    <h1>March Madness Mon!</h1>
-  )
+  return <h1>March Madness Mon!</h1>;
 }
 
-function Team({school, name, city, state}: {school: string, name: string, city: string, state: string}) {
-
+function Team({
+  school,
+  name,
+  city,
+  state,
+}: {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}) {
   return (
     <>
       <img />
       <h2>{school}</h2>
       <h3>Mascot: {name}</h3>
-      <h3>Location: {city}, {state}</h3>
+      <h3>
+        Location: {city}, {state}
+      </h3>
     </>
-  )
+  );
 }
 
 function TeamList() {
-
   return (
     <>
-     {
-        teamNames.map((singleTeam) => (
-          <Team {...singleTeam} />
-        ))
-     }
+      {teamNames.map((singleTeam) => (
+        <Team {...singleTeam} />
+      ))}
     </>
-  )
+  );
 }
 
 function App() {
-
-
   return (
     <>
       <Welcome />
       <TeamList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
